66114c6cd263d89ab72ca95b46345b44fc7164bf
Sahar-ch <sahar68.chegeni@gmail.com>
2022-08-30 09:14:17 -0400

    database setup

---
+++ travel-project/src/Data.js
@@ -0,0 +1,31 @@
+const data = [
+  {
+    image: "https://source.unsplash.com/WLxQvbMyfas",
+    location: "Japan",
+    Gmap: "https://goo.gl/maps/6PvFocYBi8qoK1zQA",
+    title: "Mount Fuji",
+    date: "01 Jan, 2020 - 14 Jan, 2020",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit a ipsa nam, minima quod explicabo fuga dolore ad eos quidem perspiciatis quisquam aliquid tempora unde voluptatum. In recusandae doloremque ab!",
+  },
+  {
+    image: "https://source.unsplash.com/JmuyB_LibRo",
+    location: "Australia",
+    Gmap: "https://goo.gl/maps/zUUdse9XMjhqnQV99",
+    title: "Sydney Opera House",
+    date: "01 Jun, 2020 - 14 Jun, 2020",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit a ipsa nam, minima quod explicabo fuga dolore ad eos quidem perspiciatis quisquam aliquid tempora unde voluptatum. In recusandae doloremque ab!",
+  },
+  {
+    image: "https://source.unsplash.com/3PeSjpLVtLg",
+    location: "Norway",
+    Gmap: "https://goo.gl/maps/QYBeE26ahuMNwppm9",
+    title: "GeirangerFjord",
+    date: "01 Oct, 2020 - 14 Oct, 2020",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit a ipsa nam, minima quod explicabo fuga dolore ad eos quidem perspiciatis quisquam aliquid tempora unde voluptatum. In recusandae doloremque ab!",
+  },
+];
+
+export default data;
